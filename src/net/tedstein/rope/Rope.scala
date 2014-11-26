28c7012ea353a26ce5184daf1dde4ff79ca249a9
Ruba Merza <ruba.merza@gmail.com>
2014-11-26 06:47:04 +0300

    moving to lwjgl3 with problems

--- src/net/tedstein/rope/Rope.scala
+++ src/net/tedstein/rope/Rope.scala
@@ -1,65 +1,105 @@
 package net.tedstein.rope
 //rubamerza blorp
 
-import org.lwjgl.LWJGLException
-import org.lwjgl.input.Keyboard
-import org.lwjgl.input.Mouse
-import org.lwjgl.opengl.Display
-import org.lwjgl.opengl.DisplayMode
+import java.nio.ByteBuffer
+
+import org.lwjgl.Sys
+import org.lwjgl.opengl.{GLContext, GL11}
+import org.lwjgl.opengl.GL11.{glClearColor, glClear}
+import org.lwjgl.system.MemoryUtil
+import org.lwjgl.system.glfw._
+import org.lwjgl.system.glfw.GLFW.{glfwInit, glfwTerminate, glfwCreateWindow, glfwDestroyWindow, glfwSetWindowPos}
+import org.lwjgl.system.glfw.GLFW.{glfwSetErrorCallback, glfwWindowShouldClose, glfwDefaultWindowHints, glfwWindowHint, glfwGetVideoMode}
+import org.lwjgl.system.glfw.GLFW.{glfwMakeContextCurrent, glfwSwapInterval, glfwShowWindow, glfwGetPrimaryMonitor, glfwSetWindowShouldClose}
+import org.lwjgl.system.glfw.GLFW.{glfwSwapBuffers, glfwPollEvents}
+import org.lwjgl.system.glfw.GLFW.{GLFW_RESIZABLE, GLFW_VISIBLE, GLFW_KEY_ESCAPE, GLFW_RELEASE}
+import org.lwjgl.opengl.GL11.{GL_TRUE, GL_FALSE, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT}
 
 
 object Rope {
+
   def main(args: Array[String]) = {
     println("Up and running!")
-    startDisplay()
+    execute()
   }
 
-  def startDisplay() {
+  def execute(): Unit = {
+    System.out.println("Hello LWJGL " + Sys.getVersion() + "!")
     try {
-      Display.setDisplayMode(new DisplayMode(800, 600))
-      Display.create()
-    } catch {
-      case e: LWJGLException => e.printStackTrace()
+      println("WHAT THE WHAT")
+      val window: Long = init()
+      loop(window)
+      glfwDestroyWindow(window)
+    } finally {
+      println("BLORP")
+      glfwTerminate()
     }
-     while (!Display.isCloseRequested) {
-      pollInput()
-      Display.update()
-    }
-
-    Display.destroy()
   }
 
-  def pollInput() {
-    if (Mouse.isButtonDown(0)) {
-      val x = Mouse.getX
-      val y = Mouse.getY
-      System.out.println("Mouse Down @ X: " + x + "Y: " + y)
-    }
-    if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-      System.out.println("SPACE KEY IS DOWN")
-    }
-    while (Keyboard.next()) {
-      if (Keyboard.getEventKeyState) {
-        if (Keyboard.getEventKey == Keyboard.KEY_A) {
-          System.out.println("A Key Pressed")
-        }
-        if (Keyboard.getEventKey == Keyboard.KEY_S) {
-          System.out.println("S Key Pressed")
-        }
-        if (Keyboard.getEventKey == Keyboard.KEY_D) {
-          System.out.println("D Key Pressed")
-        }
-      } else {
-        if (Keyboard.getEventKey == Keyboard.KEY_A) {
-          System.out.println("A Key Released")
-        }
-        if (Keyboard.getEventKey == Keyboard.KEY_S) {
-          System.out.println("S Key Released")
-        }
-        if (Keyboard.getEventKey == Keyboard.KEY_D) {
-          System.out.println("D Key Released")
-        }
+  def init(): Long = {
+    glfwSetErrorCallback(ErrorCallback.Util.getDefault())
+    if (glfwInit() != GL11.GL_TRUE)
+      throw new IllegalStateException("Unable to initialize GLFW")
+
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_VISIBLE, GL_FALSE)
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE)
+
+    val WIDTH = 300
+    val HEIGHT = 300
+    println("before create window")
+    val title: CharSequence = "Hello World"
+    val monitor: java.lang.Long = null
+    val share: java.lang.Long = null
+
+    //RUBA: things are unhappy when this call is made, also the output log shows an error related to WindowCallback below
+    //oh I got the MemoryUtil.NULL thing from an lwjgl online forum. Multiple people made the call using it.
+    //just switch both MemoryUtils with the variables monitor and share and run it to get the java.lang.NullPointerException
+    val window: Long = GLFW.glfwCreateWindow(WIDTH, HEIGHT, "HELLO WORLD", MemoryUtil.NULL, MemoryUtil.NULL)
+
+    println("after create window")
+    if (window == null)
+      throw new RuntimeException("Failed to create the GLFW window")
+
+    //RUBA: there's something wrong here, too, I think.
+    /*
+    Exception in thread "main" java.lang.NoClassDefFoundError: com/lmax/disruptor/WaitStrategy
+    at org.lwjgl.system.glfw.WindowCallback.set(WindowCallback.java:113)
+    at org.lwjgl.system.glfw.WindowCallback.set(WindowCallback.java:92)
+    at org.lwjgl.system.glfw.GLFW.glfwCreateWindow(GLFW.java:1011)
+    at net.tedstein.rope.Rope$.init(Rope.scala:57)
+    at net.tedstein.rope.Rope$.execute(Rope.scala:31)
+    at net.tedstein.rope.Rope$.main(Rope.scala:24)
+    */
+    WindowCallback.set(window, new WindowCallbackAdapter() {
+      override
+      def key(window: Long, pressed_key: Int, scancode: Int, action: Int, mods: Int)  {
+        if (pressed_key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+          glfwSetWindowShouldClose(window, GL_TRUE)
       }
+    })
+
+    val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+    glfwSetWindowPos(
+      window,
+      (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+      (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+    )
+
+    glfwMakeContextCurrent(window)
+    glfwSwapInterval(1)
+    glfwShowWindow(window)
+    return window
+  }
+
+  def loop(window: Long) {
+    GLContext.createFromCurrent()
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+      glfwSwapBuffers(window)
+      glfwPollEvents()
     }
+
   }
 }
