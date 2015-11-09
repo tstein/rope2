9234018573cf3d063780111a8c0b63d5d762a8b0
Ruba Merza <ruba.merza@gmail.com>
2015-11-09 20:20:56 +0300

    Add initial Vector4f class + delete unnecessary native files

--- src/net/tedstein/rope/Vector4f.scala
+++ src/net/tedstein/rope/Vector4f.scala
@@ -1,8 +1,50 @@
 package net.tedstein.rope
 
-/**
- * Created by ruba on 11/2/15.
- */
-class Vector4f {
+case class Vector4f(x: Float = 0.0f, y: Float = 0.0f, z: Float = 0.0f, w: Float = 0.0f) {
+
+  def lengthSquared(): Float ={
+    x * x + y * y + z * z + w * w
+  }
+
+  def length(): Float = {
+    Math.sqrt(lengthSquared()).toFloat
+  }
+
+  def add(other: Vector4f): Vector4f = {
+    val res = Vector4f(x + other.x, y + other.y, z + other.z, w + other.w)
+    println(res.x)
+    res
+  }
+
+  def subtract(other: Vector4f): Vector4f = {
+    Vector4f(x - other.x, y - other.y, z - other.z, w - other.w)
+  }
+
+  def scale(scalar: Float): Vector4f = {
+    Vector4f(x * scalar, y * scalar, z * scalar, w * scalar)
+  }
+
+  def divide(scalar: Float): Vector4f = {
+    scale(1 / scalar)
+  }
+
+  def negate(): Vector4f = {
+    scale(-1.0f)
+  }
+
+  def normalize(): Vector4f = {
+    divide(length())
+  }
+
+  def dot(other: Vector4f): Float = {
+    x * other.x + y * other.y + z * other.z + w * other.z
+  }
+
+
+  def lerp(other: Vector4f, value: Float): Vector4f = {
+    this.scale(1.0f - value).add(other.scale(value))
+  }
+
 
 }
+
