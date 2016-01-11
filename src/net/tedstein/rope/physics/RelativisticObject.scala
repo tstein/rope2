f850ee4b4fa7dadb1f485c67e52e7a1de664ef7f
Matthew Nubbe <matthewnubbe@gmail.com>
2016-01-10 19:42:16 -0800

    Correct RO.velrms to RO.velrss.
    Refactor velrms to velrss (root sum squared, not mean)
    Remove /3 in velrss calculation
    Correct the values in all test cases.
    Side note: this was effectively c=3 before.

--- src/net/tedstein/rope/physics/RelativisticObject.scala
+++ src/net/tedstein/rope/physics/RelativisticObject.scala
@@ -9,11 +9,11 @@ import scala.math.{pow, sqrt}
  * An actor in our little universe.
  */
 case class RelativisticObject(var pos: Position, var vel: Velocity, var time: Double) {
-  def velrms: Double = {
-    sqrt(List(vel.x, vel.y, vel.z).map(pow(_, 2)).sum / 3)
+  def velrss: Double = {
+    sqrt(List(vel.x, vel.y, vel.z).map(pow(_, 2)).sum)
   }
 
   def gamma: Double = {
-    1 / sqrt(1 - pow(velrms, 2))
+    1 / sqrt(1 - pow(velrss, 2))
   }
 }
--- tst/net/tedstein/rope/physics/RelativisticObjectSuite.scala
+++ tst/net/tedstein/rope/physics/RelativisticObjectSuite.scala
@@ -7,25 +7,25 @@ import net.tedstein.rope.physics.Dimensions.{Position, Velocity}
 class RelativisticObjectSuite extends RopeSuite {
   test("stationary object") {
     val obj = new RelativisticObject(new Position(0, 0, 0), new Velocity(0, 0, 0), 0)
-    assert(obj.velrms == 0.0)
+    assert(obj.velrss == 0.0)
     assert(obj.gamma == 1.0)
   }
 
   test("object moving in one dimension") {
     val obj = new RelativisticObject(new Position(0, 0, 0), new Velocity(.1, 0, 0), 0)
-    assertAlmostEquals(obj.velrms, .0577, epsilon(4))
-    assertAlmostEquals(obj.gamma, 1.0017, epsilon(4))
+    assertAlmostEquals(obj.velrss, .1, epsilon(4))
+    assertAlmostEquals(obj.gamma, 1.0050, epsilon(4))
   }
 
   test("object moving in three dimensions") {
     val obj = new RelativisticObject(new Position(0, 0, 0), new Velocity(.3, .4, .5), 0)
-    assertAlmostEquals(obj.velrms, .4082, epsilon(4))
-    assertAlmostEquals(obj.gamma, 1.0954, epsilon(4))
+    assertAlmostEquals(obj.velrss, .70711, epsilon(4))
+    assertAlmostEquals(obj.gamma, 1.4142, epsilon(4))
   }
 
   test("object hauling ass") {
     val obj = new RelativisticObject(new Position(0, 0, 0), new Velocity(.52, .604, .6039), 0)  // magnitude 0.999956
-    assertAlmostEquals(obj.velrms, .5773, epsilon(4))
-    assertAlmostEquals(obj.gamma, 1.2247, epsilon(4))
+    assertAlmostEquals(obj.velrss, 0.999956, epsilon(4))
+    assertAlmostEquals(obj.gamma, 106.12507, epsilon(4))
   }
 }
