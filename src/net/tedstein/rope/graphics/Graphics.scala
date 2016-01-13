3e31879d37292ec87214ee1a392766fc9df03e80
Ruba Merza <ruba.merza@gmail.com>
2016-01-13 12:02:33 +0300

    Fix openGL context version

--- src/net/tedstein/rope/graphics/Graphics.scala
+++ src/net/tedstein/rope/graphics/Graphics.scala
@@ -94,7 +94,7 @@ class Graphics(val universe: Universe) extends StrictLogging {
       throw new IllegalStateException("Unable to initialize GLFW")
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
     glfwWindowHint(GLFW_VISIBLE, GL_FALSE)
