5eab58ce4a26aba39fbf4a0546d16a4a77ddda90
Matthew Nubbe <matthewnubbe@gmail.com>
2016-01-17 14:54:16 -0800

    Move velrss and gamma definitions into dimensions

--- src/net/tedstein/rope/physics/Dimensions.scala
+++ src/net/tedstein/rope/physics/Dimensions.scala
@@ -1,5 +1,7 @@
 package net.tedstein.rope.physics
 
+import scala.math.{pow, sqrt}
+
 object Dimensions {
   case class Position(x: Double, y: Double, z: Double) {
     def add(vel: Velocity, elapsed: Double): Position = {
@@ -10,7 +12,14 @@ object Dimensions {
     }
   }
 
-  case class Velocity(x: Double, y: Double, z: Double)
+  case class Velocity(x: Double, y: Double, z: Double) {
+    def velrss: Double = {
+      sqrt(List(x, y, z).map(pow(_, 2)).sum)
+    }
+    def gamma: Double = {
+      1 / sqrt(1 - pow(velrss, 2))
+    }
+  }
 
   val Origin = Position(0.0, 0.0, 0.0)
   val Stationary = Velocity(0.0, 0.0, 0.0)
--- src/net/tedstein/rope/physics/RelativisticObject.scala
+++ src/net/tedstein/rope/physics/RelativisticObject.scala
@@ -9,11 +9,6 @@ import scala.math.{pow, sqrt}
  * An actor in our little universe.
  */
 case class RelativisticObject(var pos: Position, var vel: Velocity, var time: Double) {
-  def velrss: Double = {
-    sqrt(List(vel.x, vel.y, vel.z).map(pow(_, 2)).sum)
-  }
-
-  def gamma: Double = {
-    1 / sqrt(1 - pow(velrss, 2))
-  }
+  def velrss: Double = vel.velrss
+  def gamma: Double = vel.gamma
 }
