2f230c2f16af6f2a76a83424d62a57d480ea5298
Ruba Merza <ruba.merza@gmail.com>
2016-01-19 14:08:32 +0300

    Fixing obj loader functions + Mesh class

--- src/net/tedstein/rope/graphics/OBJLoader.scala
+++ src/net/tedstein/rope/graphics/OBJLoader.scala
@@ -1,6 +1,7 @@
 package net.tedstein.rope.graphics
 
 import com.typesafe.scalalogging.StrictLogging
+
 import scala.io.Source
 /**
   * Created by ruba on 1/16/16.
@@ -8,64 +9,47 @@ import scala.io.Source
 
 object OBJLoader extends StrictLogging {
   def parseObjFile(filename: String): Unit = {
-    val model = Model()
-
+  val model = Mesh()
     try {
       for (line <- Source.fromFile(filename).getLines()) {
-        val words = line.split(" ")
-        println(words(0))
-        if (words(0) == "#") {
-        } else if (words(0) == "v") {
-          println("v")
-          model.vertices = extractVertices(words.tail)
-        } else if (words(0) == "vn") {
-          model.normals = extractNormals(words.tail)
-        } else if (words(0) == "vt") {
-          model.texCoords = extractTexCoords(words.tail)
-        } else if (words(0) == "f") {
-          model.faces = extractFaces(words.tail)
+        val values = line.split(" ")
+        //println(values(0))
+        if (values(0) == "#") {
+        } else if (values(0) == "v") {
+            model.vertices ++= extractVertices(values.tail)
+        } else if (values(0) == "vn") {
+            model.normals ++= extractVertices(values.tail)
+        } else if (values(0) == "vt") {
+            model.texCoords ++= extractVertices(values.tail)
+        } else if (values(0) == "f") {
+            //model.faces ++= extractVertices(values.tail)
         }
       }
+
+    println("verts: " + model.vertices)
     } catch {
       case ex: Exception => throw ex
     }
   }
 
-  def extractVertices(words: Array[String]): Array[Float] = {
-    val verts = Array[Float](words.length)
-    val i = 0
-    for (word <- words) {
-      verts.update(i, word.toFloat)
-    }
-    verts
+  def extractVertices(values: Array[String]): Array[Float]= {
+     values.map(v => v.toFloat)
   }
 
-  def extractNormals(words: Array[String]): Array[Float] = {
-    val normals = Array[Float](words.length)
-    val i = 0
-    for (word <- words) {
-      normals.update(i, word.toFloat)
-
-    }
-    normals
+  def extractNormals(values: Array[String]): Array[Float] = {
+    values.map(v => v.toFloat)
   }
 
-  def extractTexCoords(words: Array[String]): Array[Float] = {
-    val texCoords = Array[Float](words.length)
-    val i = 0
-    for (word <- words) {
-      texCoords.update(i, word.toFloat)
-    }
-    texCoords
+  def extractTexCoords(values: Array[String]): Array[Float] = {
+    values.map(v => v.toFloat)
   }
 
-  def extractFaces(words: Array[String]): Array[Int] = {
-    val faces = Array[Int](words.length)
-    val i = 0
-    for (word <- words) {
-      faces.update(i, word.toInt)
-    }
-    faces
+  def extractFaces(values: Array[String]): Array[Int] = {
+    //line looks like this:
+    //#f v1/vn1/vt1
+    //
+    values.map(v => v.toInt)
+
   }
 
 }
