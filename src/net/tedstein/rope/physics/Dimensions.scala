78785777e07fc12160bde6274cd7d3e35306959d
Matthew Nubbe <matthewnubbe@gmail.com>
2016-02-13 15:22:36 -0800

    Cleanup in Dimensions, aisle all active code

--- src/net/tedstein/rope/physics/Dimensions.scala
+++ src/net/tedstein/rope/physics/Dimensions.scala
@@ -5,7 +5,8 @@ import scala.math.{pow, sqrt}
 
 object Dimensions {
   case class Position(v: Vector3d) {
-    //Preserve compatibility with this.x, etc.
+    //Preserve get compatibility with this.x, etc.
+    //TODO set
     def x: Double = this.v.x
     def y: Double = this.v.y
     def z: Double = this.v.z
@@ -13,17 +14,13 @@ object Dimensions {
 
     def add(pos: Position): Position = Position(pos.v + v)
 
-    def add(vel: Velocity, elapsed: Double): Position = {
-      val newX = this.x + (vel.x * elapsed)
-      val newY = this.y + (vel.y * elapsed)
-      val newZ = this.z + (vel.z * elapsed)
-      Position(newX, newY, newZ)
-    }
+    def add(vel: Velocity, elapsed: Double): Position = this.add(Position(vel.v * elapsed))
 
     //Lorentz Transform
     //https://en.wikipedia.org/wiki/Lorentz_transformation#Vector_transformations
     //Note: time is also affected by this.  It is left out here, but still an input.
-    def lorentzBoost(boostVel: Velocity, time: Double): Position = {
+    //Alternative definition
+    /*def lorentzBoost(boostVel: Velocity, time: Double): Position = {
       def f(lhs: Double, gamma: Double, dotProduct: Double, normal: Double, rhs: Double) = {
         lhs + (gamma - 1) * dotProduct * normal - rhs
       }
@@ -34,51 +31,66 @@ object Dimensions {
         f(this.y, gamma, dotProduct, boostVel.direction.y, gamma * time * boostVel.y),
         f(this.z, gamma, dotProduct, boostVel.direction.z, gamma * time * boostVel.z)
       )
+    }*/
+    //Vector definition
+    def lorentzBoost(boostVel: Velocity, time: Double): Position = {
+      val gamma = boostVel.gamma
+      Position(
+        this.v +
+          boostVel.direction.v * ((gamma - 1) * (this.v * boostVel.direction.v)) -
+          boostVel.v * gamma * time
+      )
     }
     def dot(pos: Position): Double = v * pos.v
   }
   object Position{
     //This allows a call like the following without "new":
     //Position(x,y,z)
-    def apply(xc: Double, yc: Double, zc: Double): Position = Position(Vector3d(xc,yc,zc))
+    def apply(xc: Double, yc: Double, zc: Double): Position = Position(Vector3d(xc, yc, zc))
   }
 
-  case class Velocity(x: Double, y: Double, z: Double) {
+  case class Velocity(v: Vector3d) {
+    //Preserve get compatibility with this.x, etc.
+    //TODO set
+    def x: Double = this.v.x
+    def y: Double = this.v.y
+    def z: Double = this.v.z
+    def this(xc: Double, yc: Double, zc: Double) = this(Vector3d(xc, yc, zc))
+
     //Relativistic velocity addition
     //https://en.wikipedia.org/wiki/Lorentz_transformation#Transformation_of_velocities
     //https://en.wikipedia.org/wiki/Velocity-addition_formula
     //Best way to think about the operation: take this and boost it by boostVel
-    def add(boostVel: Velocity): Velocity = {
+    //Alternative definition
+    /*def add(boostVel: Velocity): Velocity = {
       val dotProduct = this.dot(boostVel)
       def f(lhs: Double, rhs: Double): Double = {
         return (rhs + lhs / boostVel.gamma + dotProduct * rhs * boostVel.gamma / (1 + boostVel.gamma)) / (1 + dotProduct)
       }
       Velocity(f(this.x, boostVel.x), f(this.y, boostVel.y), f(this.z, boostVel.z))
       //Ideally, check that this velrss is less than 1 unless we are tacheoning
+    }*/
+    //Vector definition
+    def add(boostVel: Velocity): Velocity = {
+      val gamma = this.gamma
+      val answer = this.v + boostVel.v / gamma +
+        this.v * (this.v * boostVel.v) * (gamma / (1 + gamma))
+      Velocity(answer / (1 + this.v * boostVel.v))
     }
-    def dot(vel: Velocity): Double = {
-      var ans = vel.x * this.x
-      ans += vel.y * this.y
-      ans += vel.z * this.z
-      ans
-    }
+    def dot(vel: Velocity): Double = v * vel.v
 
-    //Divide each component by velrss, and return
-    def direction: Position = {
-      //If length is zero, just return ANY vector
-      if(this.velrss==0)
-        Position(1,0,0)
-      else
-        Position(this.x/this.velrss,this.y/this.velrss,this.z/this.velrss)
-    }
+    def direction: Position = Position(v.normalize)
     //def toarray: Array[Double] = val Array[this.x, this.y, this.z]
-    def velrss: Double = {
-      sqrt(List(x, y, z).map(pow(_, 2)).sum)
-    }
+    def velrss: Double = this.v.length
 
     def gamma: Double = {
       1 / sqrt(1 - pow(velrss, 2))
     }
+  }
+  object Velocity{
+    //This allows a call like the following without "new":
+    //Velocity(x,y,z)
+    def apply(xc: Double, yc: Double, zc: Double): Velocity = Velocity(Vector3d(xc, yc, zc))
   }
 
   val Origin = Position(0.0, 0.0, 0.0)
