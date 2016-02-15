6f5c8ff44eca9d6bc4a98ced90debe963f182ed0
Matthew Nubbe <matthewnubbe@gmail.com>
2016-02-15 12:33:20 -0800

    Moons can now orbit in ellipses out of the normal plane.

--- src/net/tedstein/rope/Universe.scala
+++ src/net/tedstein/rope/Universe.scala
@@ -1,7 +1,8 @@
 package net.tedstein.rope
 
-import net.tedstein.rope.physics.{Center, SimpleOrbiter, RelativisticObject, Dimensions}
+import net.tedstein.rope.physics.{Center, SimpleOrbiter, Orbiter, RelativisticObject, Dimensions}
 import Dimensions.{Velocity, Position}
+import net.tedstein.rope.physics.Vector3d
 
 import scala.collection.immutable.Stream.Empty
 import scala.util.Random
@@ -36,6 +37,7 @@ object Universe {
           initialSatellites = Dimensions.Empty,
           mass = 0.07/5)
         val moons = for (_ <- 0 to 2) yield {
+          if(math.random > 0.3)
           new SimpleOrbiter(
             primary = planet,
             orbitalDistance = 0.1 + .2 * random.nextFloat(),
@@ -46,6 +48,22 @@ object Universe {
             initialRadius = 0.01,
             initialSatellites = Dimensions.Empty,
             mass = 0.01/5)
+          else
+          new Orbiter(
+            //All but primary and semiMajorAxisLength have a default, so commented out all lines that can use it
+            primary = planet,
+            semiMajorAxisLength = 0.15 + 0.1 * random.nextFloat(),
+            eccentricity = 0.1 + 0.23 * random.nextFloat(),
+            orbitalAxis = Position(Vector3d.randomDir + Vector3d(0,0,-1)), //bias it to -z dir, same as the simple ones
+            initialPositionDirection = Dimensions.Origin, //why not, start pointing at the star
+            //majorAxisSuggestion = Position(Vector3d.randomDir),
+            //initialPos = Dimensions.Origin,
+            //initialVel = Dimensions.Stationary,
+            initialTime = Dimensions.Epoch,
+            initialRadius = 0.01,
+            //initialSatellites = Dimensions.Empty,
+            mass = 0.01/5 //not that this is big enough to notice or affects anything
+            )
         }
         planet.satellites = moons.toSet
         planet
