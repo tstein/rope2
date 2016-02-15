f17c66fc0894bba652aca0af9632455cd0ca18f5
Ted Stein <me@tedstein.net>
2016-02-14 20:51:22 -0800

    Use stb, which comes with lwjgl, to load textures.
    This works on Ossix.

--- src/net/tedstein/rope/graphics/TextureLoader.scala
+++ src/net/tedstein/rope/graphics/TextureLoader.scala
@@ -1,49 +1,38 @@
 package net.tedstein.rope.graphics
 
-import org.lwjgl.BufferUtils
-import org.lwjgl.opengl.GL12
-import javax.imageio.ImageIO
-import java.awt.image.BufferedImage
-import java.io.IOException
 import java.nio.ByteBuffer
-import java.nio.file.Files
-import java.nio.file.Paths
+
+import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.GL11._
+import org.lwjgl.opengl.GL12
+import org.lwjgl.stb.STBImage.stbi_load
+
+case class LoadedImage(bytes: ByteBuffer, width: Int, height: Int)
 
 object TextureLoader {
   private val BytesPerPixel = 4
   private val TextureRoot = "assets"
 
-  def loadTexture(image: BufferedImage): Int = {
-    val pixels = new Array[Int](image.getWidth * image.getHeight)
-    image.getRGB(0, 0, image.getWidth, image.getHeight, pixels, 0, image.getWidth)
-    val buffer = BufferUtils.createByteBuffer(image.getWidth * image.getHeight * BytesPerPixel)
-
-    for (y <- 0 until image.getHeight;
-         x <- 0 until image.getWidth) {
-      val pixel = pixels(y * image.getWidth + x)
-      buffer.put(((pixel >> 16) & 0xFF).toByte)
-      buffer.put(((pixel >> 8) & 0xFF).toByte)
-      buffer.put((pixel & 0xFF).toByte)
-      buffer.put(((pixel >> 24) & 0xFF).toByte)
-    }
-    buffer.flip
-
+  def loadTexture(image: LoadedImage): Int = {
     val textureID: Int = glGenTextures
     glBindTexture(GL_TEXTURE_2D, textureID)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth, image.getHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.bytes)
     textureID
   }
 
-  private def loadImage(path: String): BufferedImage = {
-      ImageIO.read(Files.newInputStream(Paths.get(path)))
+  private def loadImage(path: String): LoadedImage = {
+    val width = BufferUtils.createIntBuffer(1)
+    val height = BufferUtils.createIntBuffer(1)
+    val numPixels = BufferUtils.createIntBuffer(1)
+    val imageBytes = stbi_load(path, width, height, numPixels, BytesPerPixel)
+    LoadedImage(imageBytes, width.get(0), height.get(0))
   }
 
-  def loadImages(textureNames: Seq[String]): Map[String, BufferedImage] = {
-    textureNames.par.map(name => name -> loadImage(s"$TextureRoot/$name.jpg")).toMap.seq
+  def loadImages(textureNames: Seq[String]): Map[String, LoadedImage] = {
+    textureNames.map(name => name -> loadImage(s"$TextureRoot/$name.jpg")).toMap.seq
   }
 }
